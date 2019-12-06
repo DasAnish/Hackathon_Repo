1afd85a0e2692ba6bc6854aa9a69710e4d8a5b80
NoNotCar <13haslat@gmail.com>
2019-12-06 16:23:30 +0000

    give games pls

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 local testgame = require("asteroids")
-local screensize={512,384}
+local screensize={640,480}
+local games={testgame}
 function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest")
-  love.window.setMode(unpack(screensize))
+  love.window.setMode(screensize[1]*#games,screensize[2])
   testgame.setup(screensize)
   frame1=love.graphics.newImage("frame1.png")
   frame2=love.graphics.newImage("frame2.png")
+  frame3=love.graphics.newImage("frame3.png")
 end
 
 function love.update(dt)
@@ -13,6 +15,8 @@ function love.update(dt)
 end
 
 function love.draw()
-  testgame.render({0,0},screensize)
-  love.graphics.draw(frame1,0,0,0,4,4)
+  for i,g in ipairs(games) do
+    g.render({(i-1)*screensize[1],0},screensize)
+    love.graphics.draw(frame1,(i-1)*screensize[1],0,0,5,5)
+  end
 end
